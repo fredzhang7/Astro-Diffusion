e02e13ea6375a4591303300d862466dcead338a0
Fred <freddyzhang666@gmail.com>
2022-11-02 23:44:12 -0700

    beta test 1/2

---
+++ astro.py
@@ -0,0 +1,141 @@
+from types import SimpleNamespace
+import os, time, random, gc, torch
+from deforum_stable_diffusion import render_image_batch
+from super_res import get_optimized_prompts, get_optimized_model_choice
+
+
+def get_output_folder(output_path, batch_folder):
+    out_path = os.path.join(output_path, time.strftime('%Y-%m'))
+    if batch_folder != "":
+        out_path = os.path.join(out_path, batch_folder)
+    os.makedirs(out_path, exist_ok=True)
+    return out_path
+
+
+model_checkpoints = {
+    # Stable Diffusion Models
+    "sd-v1-5-full-ema.ckpt":      "7.2 GB, latest, highest resolution SD, high VRAM",
+    "sd-v1-5.ckpt":               "4.0 GB, latest, higher resolution SD, medium VRAM",
+    "sd-v1-1-full-ema.ckpt":      "7.2 GB, lower resolution, medium VRAM",
+    "sd-v1-1.ckpt":               "4.0 GB, lowest resolution, medium VRAM",
+
+    # Animation Diffusion Models
+    "waifu-diffusion-v1-3.ckpt":  "2.0 GB, high-quality waifu characters, low VRAM",
+    "disney-diffusion-v1.ckpt":   "2.0 GB, high-quality Disney characters, animals, cars, & landscapes, low VRAM",
+
+    # Robo Diffusion Models
+    "robo-diffusion-v1.ckpt":     "4.0 GB, high-quality robot-like images, medium VRAM",
+}
+
+
+def AstroArgs():
+    # Model Settings
+    model_checkpoint = "waifu-diffusion-v1-3.ckpt"    # one of "custom", a key in model_checkpoints (above)
+    check_sha256 = False                 # whether to check the sha256 hash of the checkpoint file. set to True if having issues with model downloads
+    custom_config_path = ""              # if model_checkpoint "custom", path to a custom model config yaml file. else ""
+    custom_checkpoint_path = ""          # if model_checkpoint "custom", path to custom checkpoint file. else ""
+    allow_nsfw = False                   # whether to allow nsfw images. set to True if you are 18+ and want to use nsfw images
+
+    # Image Settings
+    W = 640                             # image width
+    H = 640                             # image height
+    W, H = map(lambda x: x - x % 64,     # ensure that shape is divisable by 64
+               (W, H))
+
+    # Sampling Settings
+    seed = -1                            # random seed
+    sampler = "klms"                     # one of "klms", "dpm2", "dpm2_ancestral", "heun", "euler", "euler_ancestral", "ddim"
+    steps = 50                           # number of steps to run
+    scale = 8                            # guidance scale (0: 4x4, 1: 8x8, ..., 7: 512x512, 8: 1024x1024)
+    ddim_eta = 0.0                       # amount of ddim to use (0.0: no ddim, 1.0: full ddim)
+    dynamic_threshold = None             # adaptive threshold for dpm2
+    static_threshold = None              # static threshold for dpm2
+
+    # Save & Display Settings
+    save_samples = True                  # whether to save samples to disk
+    save_settings = True                 # whether to save settings to a file
+    display_samples = True               # whether to display samples in Colab
+    save_sample_per_step = False         # whether to save samples per step or only the last one (only for ddim)
+    show_sample_per_step = False         # whether to show samples for each step
+
+    # Prompt Settings
+    prompt_weighting = False             # one of False, "linear", "exponential"
+    normalize_prompt_weights = True      # whether to normalize prompt weights to sum to 1
+    log_weighted_subprompts = False      # whether to log the weighted subprompts
+
+    # Batch Settings
+    n_batch = 1                          # number of samples to generate in parallel
+    output_path = "./"                   # folder path to save images to
+    batch_name = "StableFun"             # subfolder name to save images to
+    seed_behavior = "random"             # one of "iter", "fixed", "random"
+    make_grid = False                    # whether to make a grid of images
+    grid_rows = 2                        # number of rows in grid
+    filename_format = "{timestring}_{index}_{prompt}.png"
+    outdir = get_output_folder(output_path, batch_name)
+
+    # Init Settings
+    use_init = False 
+    strength = 0                         # a float between 0 and 1
+    strength_0_no_init = True            # if True, strength becomes 0 when init is not used
+    init_image = ""                      # URL or local path to image
+    use_mask = False                     # whether to use a mask. whiter pixels are masked out
+    use_alpha_as_mask = False            # use the alpha channel of the image as a mask
+    mask_file = "https://www.filterforge.com/wiki/images/archive/b/b7/20080927223728%21Polygonal_gradient_thumb.jpg" 
+    invert_mask = False                  # whether to invert the mask
+    mask_brightness_adjust = 1.0         # 0.0 is black, 1.0 is no change, 2.0 is white
+    mask_contrast_adjust = 1.0           # 0.0 is min contrast, 1.0 is no change, 2.0 is max contrast
+    overlay_mask = True                  # masked image overlay at the end of the generation so it does not get degraded by encoding and decoding
+    mask_overlay_blur = 5                # bluring edges of final overlay mask. 0 is no blur, 1 is a little blur, 5 is a lot of blur
+
+    # Noise Settings
+    n_samples = 1                        # number of samples to generate
+    precision = 'autocast'               # one of 'autocast', 'fp32', 'fp16'
+    C = 4                                # number of channels
+    f = 8                                # number of features
+
+    prompt = ""                          # optional prompt
+    timestring = ""                      # time string for output file name
+    init_latent = None                   # if not None, use this latent as the starting point for generation
+    init_sample = None                   # if not None, use this image as the initial sample
+    init_c = None                        # if not None, use this c vector as the starting point for generation
+
+    return locals()
+
+
+args = SimpleNamespace(**AstroArgs())
+args.time = str(round(time.time()))
+args.strength = max(0.0, min(1.0, args.strength))
+
+if args.seed == -1:
+    args.seed = random.randint(0, 2**32 - 1)
+if not args.use_init:
+    args.init_image = None
+if args.sampler != 'ddim':
+    args.ddim_eta = 0
+
+# clean up unused memory
+gc.collect()
+torch.cuda.empty_cache()
+
+
+"""
+    If a theme doesn't appear in the list, pass "default" to get_optimized_prompts(). Lowercase works too.
+
+    Themes:
+     - Default
+     - Anime Girl (anime girl, waifu)
+     - Anime Boy (anime boy, husbando)
+     - Disney (disney princess, disney character, disney villain, disney animal, animated car, animated landscape)
+     - Space (universe, supernova, black hole, planet, galaxy, nebula, star, astronaut, rocket, spaceship, alien, night sky)
+     - Robot (robot, android, cyborg, mecha)
+     - Low Res (low resolution, pixel art)
+
+    Examples:
+     1. prompts = get_optimized_prompts(prompt_source='./anime_girls.txt', theme='anime girl')
+     2. prompts = ['bear at a lake, magical energies emanating from it, god rays, wide angle, fantasy art, matte painting, sharp focus, vibrant colors, high contrast, illustration, art by justin gerard']
+
+"""
+
+prompts = []
+
+render_image_batch(args, prompts, upscale_ratio=2)
---
+++ super_res.py
@@ -0,0 +1,113 @@
+from PIL import Image
+from cv2 import dnn_superres
+from typing import Tuple
+import cv2, os, random, numpy
+
+
+def upscale_image(image: Image, scale: int) -> Image:
+    """
+    Upscale an image using OpenCV's EDSR model
+    """
+    sr = dnn_superres.DnnSuperResImpl_create()
+    if isinstance(image, str):
+        image = cv2.imread(image)
+    else:
+        image = cv2.cvtColor(numpy.array(image), cv2.COLOR_RGB2BGR)
+    path = "EDSR_x4.pb" if scale == 4 else "EDSR_x3.pb" if scale == 3 else "EDSR_x2.pb" if scale == 2 else None
+    if path is None:
+        raise ValueError("Scale must be 2, 3, or 4")
+    sr.readModel(path)
+    sr.setModel("edsr", scale)
+    result = sr.upsample(image)
+    return Image.fromarray(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
+
+
+def upscale_video(video_path: str, scale: int) -> None:
+    """
+    Upscale a video, frame by frame, using OpenCV's EDSR model
+    """
+    if not os.path.isfile(video_path):
+        raise ValueError("Video path is invalid")
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        raise ValueError("Video is invalid")
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter('output.mp4', fourcc, fps, (width * scale, height * scale))
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame = upscale_image(frame, scale)
+        out.write(frame)
+    cap.release()
+    out.release()
+
+
+def get_optimized_prompts(prompt_source: Tuple[str, list[str]], theme: str) -> list[str]:
+    """
+    - Read a file with each prompt in a different line, or pass an array of prompts.
+    - Pass a theme from the themes list to get a prompt that fits the theme.
+    - Returns a list of optimized prompts.
+    """
+    prompts = None
+    theme = theme.toLowerCase()
+    if not os.path.isfile(prompt_source):
+        prompts = prompt_source
+    else:
+        with open(prompt_source, 'r') as f:
+            prompts = f.readlines()
+    for prompt in prompts:
+        if "anime girl" in theme or "waifu" in theme:
+            prompt = "correct body positions, " + prompt.strip() + ", sharp focus, beautiful, attractive, 4k, 8k ultra hd"
+            if 'girls' in prompt:
+                prompts.append(prompt)
+            else:
+                if random.randint(0, 1) == 1:
+                    prompts.append("1girl, " + prompt + ", anime incarnation")
+                else:
+                    prompts.append("1woman, " + prompt + ", anime incarnation")
+        elif "anime boy" in theme or "husbando" in theme:
+            emotions = ["sad", "angry", "surprised", "disgusted", "afraid", "calm", "confused", "bored"]
+            if not any(emotion in prompt for emotion in emotions):
+                prompt = "1boy, " + prompt.strip() + ", feeling very happy, excited, joyful, cheerful, "
+                if random.randint(0, 1) == 0:
+                    prompts.append(prompt + "trending on artstation")
+                else:
+                    prompts.append(prompt + "trending on pixiv")
+        elif "nature" in theme:
+            prompt = prompt.strip() + ", trending on artstation, hyperrealistic, trending, 4 k, 8 k, uhd"
+            if not "detailed" in prompt:
+                prompt += ", highly detailed"
+            prompts.append(prompt)
+        elif "space" in theme:
+            if len(prompt) < 45:
+                prompts.append(prompt.strip() + ", octane render, masterpiece, cinematic, trending on artstation, 8k ultra hd")
+            elif not "lighting" in prompt:
+                prompts.append(prompt.strip() + ", cinematic lighting, 8k ultra hd")
+            else:
+                prompts.append(prompt.strip() + ", 8k ultra hd")
+        else:
+            prompts.append(prompt.strip())
+    return prompts
+
+
+def get_optimized_model_choice(theme: str, lightweight: bool) -> str:
+    """
+    Return the model name based on the theme and the checkpoint model size (lightweight)
+    """
+    if 'anime' in theme or 'waifu' in theme:
+        return "waifu-diffusion-v1-3.ckpt"
+    if 'robot' in theme or 'mecha' in theme or 'android' in theme or 'cyborg' in theme:
+        return "robo-diffusion-v1.ckpt"
+    
+    if lightweight:
+        if 'low res' in theme:
+            return "sd-v1-1.ckpt"
+        return 'sd-v1-5.ckpt'
+    else:
+        if 'low res' in theme:
+            return "sd-v1-1-full-ema.ckpt"
+        return "sd-v1-5-full-ema.ckpt"
